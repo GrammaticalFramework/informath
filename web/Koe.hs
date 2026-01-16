a3950ec990e12d514d06d413c58a941004ffad11
aarneranta <aarne@chalmers.se>
2026-01-16 17:06:23 +0100

    towards a web version

--- web/Koe.hs
+++ web/Koe.hs
@@ -14,16 +14,33 @@ main = do
 
 --myresponse req@(Request uri@(URI scheme auth path query fragment) method headers body) = outputText body
 myresponse req@(Request uri method headers body) =
-  outputText (unlines ["HELLLO", uriScheme uri, uriPath uri, processQuery (uriQuery uri)])
+  case method of
+    GET -> case uriQuery uri of
+      s@(_:_) -> outputHTML (unlines (startpage ["GETT", uriScheme uri, uriPath uri,  "<br>", processQuery (uriQuery uri)]))
+      _ -> outputHTML (unlines (startpage []))
+    POST -> outputHTML (unlines (startpage ["POSTT", uriScheme uri, uriPath uri, body, "<br>", processQuery body]))
 
 
 -- URI: https://hackage.haskell.org/package/network-uri-2.6.4.2/docs/Network-URI.html
 
-processQuery s = case (break (=='=') s) of
-  (_:"dk", _:exp) -> "(" ++ unEscapeString exp ++ ")"
-  (_:"num", _:exp) -> show (cube (read ( unEscapeString exp ) ))
+processQuery s = case (break (=='=') (dropWhile (=='?') s)) of
+  ("dk", _:exp) -> "(" ++ unEscapeString exp ++ ")"
+  ("num", _:exp) -> show (cube (read ( unEscapeString exp ) ))
   (_, _:v) -> reverse (unEscapeString v)
+  _ -> unwords (replicate 5 s)
 
 cube :: Int -> Int
 cube n = n*n*n
 
+startpage x = [
+  "<html>",
+  "<body>",
+  "START",
+  "<form method=\"post\" action=\"/\">",
+    "<label for=\"num\">Number:</label>",
+    "<input type=\"text\" name=\"num\" /><br />",
+    "<input type=\"submit\" />",
+  "</form>"] ++ x ++ [
+  "</body>",
+  "</html>"
+  ]
