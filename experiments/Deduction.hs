33e3d0453a6dae5b4a4427392cc73edd9a61f65d
Aarne Ranta <aarne@chalmers.se>
2026-01-30 11:18:54 +0100

    converting line sequences to trees in Deduction

--- experiments/Deduction.hs
+++ experiments/Deduction.hs
@@ -1,5 +1,7 @@
 module Deduction where
 
+import Data.List (intersperse)
+
 -- experiment with Jan von Plato 2017. "From Gentzen to Jaskowski and Back:
 -- Algorithmic Translation of Derivations Between the Two Main Systems of Natural Deduction."
 -- code partly borrowed from https://github.com/aarneranta/PESCA
@@ -15,8 +17,7 @@ aB = Atom "B"
 aC = Atom "C"
 
 main = do
-  putStrLn $ prf (And (Or aA aB) aC)
-  putStrLn $ prf (If (And aA aB) (And (Not (Not aA)) (Not (Not aB))))
+  putStrLn $ prLatexFile $ prls exLines1 ++ "\n\n" ++ prlst exLines1
 
 data Formula =
     And Formula Formula
@@ -25,7 +26,7 @@ data Formula =
   | Not Formula
   | Falsum
   | Atom String
-  deriving Show
+  deriving (Show, Eq)
 
 data Proof =
     AndI Formula Formula Proof Proof
@@ -40,8 +41,43 @@ data Proof =
   | NotE Formula Proof Proof
   | FalsumE Formula Proof
   | Hypo Int Formula
-  deriving Show
+  | Assumption Formula
+  deriving (Show, Eq)
 
+data Line = Line {
+  line :: Int,
+  context :: [Int],
+  formula :: Formula,
+  rule :: String,
+  premisses :: [Int],
+  discharged :: [Int]
+  }
+  deriving (Show, Eq)
+
+mkLine = Line
+
+exLines1 :: [Line]
+exLines1 = [
+  mkLine 1 [] (If aA aB) "Assumption" [] [],
+  mkLine 2 [2] (And aA (Not aB)) "Hypo" [] [],
+  mkLine 3 [2] aA "AndE1" [2] [],
+  mkLine 4 [2] (Not aB) "AndE2" [2] [],
+  mkLine 5 [2] aB "IfE" [1, 3] [],
+  mkLine 6 [2] Falsum "IfE" [4, 5] [],
+  mkLine 7 [] (Not (And aA (Not aB))) "IfI" [6] [2]
+  ]
+
+
+data Tree a = Tree a [Tree a]
+  deriving (Show, Eq)
+
+
+lines2tree :: [Line] -> Tree Line
+lines2tree ls = ltr (last ls) where
+  ltr concl = Tree concl [ltr (ls !! (prem-1)) | prem <- premisses concl]
+
+
+--proof2lines :: Proof -> [Line]
 
 prf :: Formula -> String
 prf = pr 0 where
@@ -57,6 +93,8 @@ prf = pr 0 where
 -- printing to LaTeX proof.sty trees
 prt :: Proof -> String
 prt proof = case proof of
+   Assumption formula ->
+     prf formula
    Hypo int formula -> 
      "\\discharge{" ++ show int ++ "}{" ++ prf formula ++ "}"
    AndI  f1 f2 p1 p2 ->
@@ -97,3 +135,37 @@ prt proof = case proof of
      prt p1 ++++ "}"
 
 
+prls :: [Line] -> String
+prls lns = unlines $
+  "\\[" :
+  "\\begin{array}{llllll}" :
+  [unwords (intersperse "&" (prl ln)) ++ "\\\\" | ln <- lns] ++
+  ["\\end{array}", "\\]"] 
+
+prl :: Line -> [String]
+prl ln = [
+  concat (replicate (length (context ln)) "\\mid"),
+  show (line ln) ++ ".",
+  prf (formula ln),
+  "\\mbox{" ++ rule ln ++ "}",
+  concat (intersperse ", " (map show (premisses ln))),
+  if null (discharged ln) then "" else "[" ++ concat (intersperse ", " (map show (discharged ln))) ++ "]"
+  ]
+
+prlst :: [Line] -> String
+prlst = mathdisplay . pr . lines2tree where
+  pr (Tree a ts) = case ts of
+    [] -> unwords (prl a)
+    _ -> "\\infer{" ++ unwords (prl a) ++ "}{" ++ unwords (intersperse "&" (map pr ts)) ++ "}"
+
+mathdisplay s = "\\[" ++ s ++ "\\]"
+
+
+prLatexFile string =
+ "\\documentstyle[proof]{article}" ++++
+ "\\setlength{\\parskip}{2mm}" ++++
+ "\\setlength{\\parindent}{0mm}" ++++
+ "\\newcommand{\\discharge}[2]{\\begin{array}[b]{c} #1 \\\\ #2 \\end{array}}" ++++
+ "\\begin{document}" ++++ 
+ string ++++ 
+ "\\end{document}"
