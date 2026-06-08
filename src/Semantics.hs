c4cfcdb4389db4b66e6839136d6205a3b400883b
Aarne Ranta <aarne@chalmers.se>
2026-06-08 15:08:22 +0200

    module for explicitly defined semantic rules

---
+++ src/Semantics.hs
@@ -0,0 +1,47 @@
+module Semantics where
+
+import Informath
+import PGF hiding (Tree)
+
+import qualified Data.Map as M
+
+type SemDefs = M.Map CId ([Expr] -> Expr)
+
+appSemDefs :: Gf a => SemDefs -> a -> a
+appSemDefs defs = fg . applySemDefs defs . gf
+
+
+applySemDefs :: SemDefs -> Expr -> Expr
+applySemDefs defs exp = case unApp exp of
+  Just (fun, args) ->
+    let args' = map (applySemDefs defs) args in case M.lookup fun defs of
+          Just fun' -> fun' args'
+          _ -> mkApp fun args'
+  _ -> exp
+
+
+mkSemDef :: Expr -> Expr -> [Expr] -> Expr
+mkSemDef a b = case unApp a of
+  Just (fun, args) -> case mapM getVar args of
+    Just xs -> mkFun xs b
+    _ -> error ("not a valid function definition " ++ showExpr [] a)
+  _ -> error ("not a valid function definition " ++ showExpr [] a)
+ where
+  getVar x = case unApp x of
+    Just (y, []) -> return y
+    _ -> Nothing
+  mkFun xs exp = \vars -> subst [(x, vars !! i) | (x, i) <- zip xs [0..]] exp
+  subst vs exp = case unApp exp of
+    Just (x, []) -> case lookup x vs of
+      Just e -> e
+      _ -> exp
+    Just (f, args) -> mkApp f (map (subst vs) args)
+    _ -> exp
+
+
+readSemDef :: String -> [Expr] -> Expr
+readSemDef s = case break (=='=') s of
+  (a, _:b) -> case (readExpr a, readExpr b) of
+    (Just a', Just b') -> mkSemDef a' b'
+    _ -> error ("cannot read semantic definition " ++ s)
+  _ -> error ("cannot parse semantic definition " ++ s)
